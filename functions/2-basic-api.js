0e091168c56c4248740fecf8bd89ebb2ca9d62d1
Scott Gruber <scottgruber21@gmail.com>
2021-02-20 13:14:10 -0500

    added access control header to API

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require('../assets/data')
 
 exports.handler = async (event, context) => {
  return {
+  headers:{
+   'Access-Control-Allow-Origin': '*'
+  }
   statusCode: 202,
   body: JSON.stringify(items)
  }
