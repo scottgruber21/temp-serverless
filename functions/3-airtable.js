965271ec06b1127eae2ed54ba180357916605d77
Scott Gruber <scottgruber21@gmail.com>
2021-02-22 18:54:15 -0500

    Added Airtable app

---
+++ functions/3-airtable.js
@@ -0,0 +1,38 @@
+require('dotenv').config
+
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('appEows8rhkxIQZh3')
+  .table('products')
+
+
+exports.handler = async(event, context, cb) => {
+  try{
+  const {records} = await airtable.list();
+  const products = records.map((product) => {
+    const {id} = product;
+    const {name, image, price} = product.fields;
+    const url = image[0].url;
+    return {id, name, url, price}
+  })
+
+  return {
+  headers:{
+   'Access-Control-Allow-Origin' : '*'
+  },
+  statusCode: 200,
+  body: JSON.stringify(products),
+ }
+}
+  
+  catch(error){
+    return {
+  headers:{
+   'Access-Control-Allow-Origin' : '*'
+  },
+  statusCode: 500,
+  body: 'Server Error',
+ }
+  }
+}
---
+++ functions/3-z-complete.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('appEows8rhkxIQZh3')
+  .table('products')
+
+exports.handler = async (event, context, cb) => {
+  const {id} = event.queryStringParameters
+  if(id){
+    try{
+      const product = await airtable.retrieve(id)
+      if(product.error){
+        return {
+          headers:{
+            'Access-Control-Allow-Origin': '*',
+          },
+          statusCode: 404,
+          body: `No product with id ${id}`
+        }
+      }
+      return {
+        headers:{
+            'Access-Control-Allow-Origin': '*',
+          },
+        statusCode: 200,
+        body: JSON.stringify(product)
+      }
+    } catch (error){
+      return {
+        headers:{
+            'Access-Control-Allow-Origin': '*',
+          },
+        statusCode: 500,
+        body: `Server error`
+      }
+    }
+  }
+try{
+  const {records} = await airtable.list();
+  const products = records.map((product) => {
+    const {id} = product;
+    const {name, image, price} = product.fields;
+    const url = image[0].url;
+    return {id, name, url, price}
+  })
+
+  return {
+    headers:{
+            'Access-Control-Allow-Origin': '*',
+          },
+  statusCode: 200,
+  body: JSON.stringify(products),
+ }
+}
+  
+  catch(error){
+    return {
+      headers:{
+            'Access-Control-Allow-Origin': '*',
+          },
+  statusCode: 500,
+  body: 'Server Error',
+ }
+  }
+}
